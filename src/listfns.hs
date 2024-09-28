740130fb33fd5c0b3162ca23aab4a0802ebd6b1a
Chris Flinn <christopher.flinn@gmail.com>
2024-09-28 15:48:53 +1000

    mooc

--- src/listfns.hs
+++
@@ -1,7 +0,0 @@
-our_length :: [a] -> Int
-our_length [] = 0
-our_length (x : xs) = 1 + our_length xs
-
-our_drop :: Int -> [a] -> [a]
-our_drop i [] = []
-our_drop i (x : xs) = (our_drop (i - 1) xs)
